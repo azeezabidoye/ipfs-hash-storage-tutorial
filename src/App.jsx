c6752bced8e7151226017fc98f571143e9b6e246
azeezabidoye <azeezabidoye@yahoo.com>
2024-11-25 08:18:44 +0100

    Component updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,16 @@ import { ethers } from "ethers";
 import { pinata } from "pinata";
 import "./App.css";
 
-function App() {}
+// Contract Address
+const contractAddress = "0x99aFF0cbBb2561d1898b6B14C46b2cb994F9eDEC";
+
+// Contract ABI
+import { abi as contractAbi } from "./artifacts/contracts/IpfsHashStorage.sol/IpfsHashStorage.json";
+
+function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [ipfsHash, setIpfsHash] = useState("");
+  const [storedHash, setStoredHash] = useState("");
+}
 
 export default App;
