deb33be2da16d093d55cc9728adf1a8bb43670e9
azeezabidoye <azeezabidoye@yahoo.com>
2024-11-25 18:33:27 +0100

    Component and config updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { ethers } from "ethers";
 import { pinata } from "./config";
 import "./App.css";
 
-// Contract Address
-const contractAddress = "0x99aFF0cbBb2561d1898b6B14C46b2cb994F9eDEC";
-
 // Contract ABI
 import { abi } from "./artifacts/contracts/IpfsHashStorage.sol/IpfsHashStorage.json";
+
+// Contract Address
+const contractAddress = "0x99aFF0cbBb2561d1898b6B14C46b2cb994F9eDEC";
 
 function App() {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -30,15 +30,44 @@ function App() {
       // Create a contract instance
       const contract = new ethers.Contract(contractAddress, abi, signer);
       try {
-        // Send the transaction to store IPFS Hash on the blockchain
+        // Send transaction to store IPFS Hash on the blockchain
         const txResponse = await contract.setIPFSHash(hash);
-        const txReceipt = await txResponse.wait(1);
-        return txReceipt;
+        await txResponse.wait();
       } catch (error) {
         console.log("Failed to store IPFS Hash on blockchain", error);
       }
     }
   };
+
+  // Function to Retrieve Hash from the Blockchain
+  const retrieveHashFromBlockchain = async () => {
+    // Check if Metamask is installed
+    if (typeof window.ethereum !== "undefined") {
+      // Connect to Ethereum provider (Metamask)
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      // Create a contract instance
+      const contract = new ethers.Contract(contractAddress, abi, signer);
+      try {
+        // Send transaction to retrieve IPFS Hash from the blockchain
+        const storedHash = await contract.getIPFSHash();
+        setStoredHash(storedHash);
+      } catch (error) {
+        console.log("Failed to retrieve IPFS Hash from blockchain", error);
+      }
+    }
+  };
+
+  //   const storeHashOnBlockchain = async (hash) => {
+  //     if (typeof window.ethereum !== "undefined") {
+  //       const provider = new ethers.providers.Web3Provider(window.ethereum);
+  //       const signer = provider.getSigner();
+  //       const contract = new ethers.Contract(contractAddress, abi, signer);
+  //       const tx = await contract.setIPFSHash(hash);
+  //       await tx.wait();
+  //     }
+  //   };
 
   // Upload Function
   const handleUpload = async () => {
@@ -53,6 +82,7 @@ function App() {
   return (
     <>
       <div className="app-container">
+        <h1>Store IPFS On-chain</h1>
         <div className="upload-section">
           <label className="form-label">Choose File</label>
           <input
@@ -67,6 +97,19 @@ function App() {
         {ipfsHash && (
           <div className="result-section">
             <p>IPFS Hash: {ipfsHash}</p>
+          </div>
+        )}
+        <div className="retrieve-section">
+          <button
+            className="retrieve-button"
+            onClick={retrieveHashFromBlockchain}
+          >
+            Retrieve Hash
+          </button>
+        </div>
+        {storedHash && (
+          <div>
+            <p>Retrieved Hash: {storedHash}</p>
           </div>
         )}
       </div>
